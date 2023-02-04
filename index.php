5fbdb21ecadae1472b3abc26d8259cdb68901c7c
LucaFaedda <lucakarl91@gmail.com>
2023-02-04 14:32:22 +0100

    aggiunto lista attori (bonus 1)

--- index.php
+++ index.php
@@ -9,11 +9,13 @@ error_reporting(E_ALL);
         public $nome;
         public $durata;
         public $voto_originale;
+        public $attori;
         
-        function __constructor($_nome, $_durata, $_voto_originale){
+        function __constructor($_nome, $_durata, $_voto_originale, $_attori){
             $this->nome = $_nome;
             $this->durata = $_durata;
             $this->voto_originale = $_voto_originale;
+            $this->attori = $_attori;
         }
 
         public function setVoto($voto){
@@ -32,19 +34,22 @@ error_reporting(E_ALL);
     $nome_1 = "Il signore degli Anelli: La compagnia dell'Anello";
     $durata_1= '2h 58m';
     $voto_originale_1 = 9;
-    $movie_1 = new Movies($nome_1,$durata_1, $voto_originale_1 );
+    $attori_1 = ['Elijan Wood', 'Ian McKellen', 'Liv Tyler', 'Viggo Mortense', 'Sean Astin', 'Cate Blanchett', 'John Rhys-Davies', 'Billy Boyd', 'Dominic Monaghan', 'Orlando Bloom', 'Christoper Lee','Hugo Weaving','Sean Bean', 'Ian Holm'. 'Andy Serkis'];
+    $movie_1 = new Movies($nome_1,$durata_1, $voto_originale_1, $attori_1 );
     $nome_2 = "Il signore degli Anelli: Le due Torri";
     $durata_2= '2h 59m';
     $voto_originale_2 = 9;
-    $movie_2 = new Movies($nome_2,$durata_2, $voto_originale_2);
+    $attori_2 = ['Elijan Wood', 'Ian McKellen', 'Liv Tyler', 'Viggo Mortense', 'Sean Astin', 'Cate Blanchett', 'John Rhys-Davies','Bernad Hill', 'Miranda Otto', 'Karl Urban', 'David Wenham','Brad Dourif', 'Billy Boyd', 'Dominic Monaghan', 'Orlando Bloom', 'Christoper Lee','Hugo Weaving', 'Andy Serkis'];
+    $movie_2 = new Movies($nome_2, $durata_2, $voto_originale_2, $attori_2);
     $nome_3 = "Il signore degli Anelli: Il ritorno Del Re";
     $durata_3= '3h 30m';
     $voto_originale_3 = 10;
-    $movie_3 = new Movies($nome_3,$durata_3, $voto_originale_3);
+    $attori_3 = ['Elijan Wood', 'Ian McKellen', 'Liv Tyler', 'Viggo Mortense', 'Sean Astin', 'Cate Blanchett', 'John Rhys-Davies','Bernad Hill', 'Miranda Otto', 'Karl Urban', 'David Wenham','Brad Dourif', 'Billy Boyd', 'Dominic Monaghan', 'Orlando Bloom', 'Christoper Lee','Hugo Weaving', 'Andy Serkis', 'Sean Bean' , 'John Noble'];
+    $movie_3 = new Movies($nome_3,$durata_3, $voto_originale_3, $attori_3);
 
-    $movie_1->setVoto( $movie_1->voto_originale); // qua posso passarli il voto originale ma solo con la freccia.
-    $movie_2->setVoto( $movie_2->voto_originale);
-    $movie_3->setVoto( $movie_3->voto_originale);
+    $movie_1->setVoto( $voto_originale_1); // qua posso passarli il voto originale ma solo con la freccia.
+    $movie_2->setVoto( $voto_originale_2);
+    $movie_3->setVoto( $voto_originale_3);
 
     // con i costrutti è più pulito il codice in HTML
 
@@ -71,18 +76,21 @@ error_reporting(E_ALL);
             <div class="col-12 mt-5">
                 <div>
                     <h2><?php  echo "Titolo:"." ".$nome_1;?></h2>
-                    <h3><?php  echo "Durata:"." ".$durata_1;?></h3>
-                    <h3><?php echo "Voto in decimi:"." ".$voto_originale_1."<br>"."Voto in quinti:"." ".$movie_1->getVoto();?></h3>
+                    <p><?php  echo "<strong class='text-danger'>Durata:</strong>"." ".$durata_1;?></p>
+                    <p><?php echo "<strong class='text-danger'>Voto in decimi:</strong>"." ".$voto_originale_1."<br>"."Voto in quinti:"." ".$movie_1->getVoto();?></p>
+                    <p><?php echo "<strong class='text-danger'>Attori:</strong>"." ".implode(", ",$attori_1); ?></p>
                 </div>
                 <div class="mt-4">
                     <h2><?php  echo "Titolo:"." ".$nome_2;?></h2>
-                    <h3><?php  echo "Durata:"." ".$durata_2;?></h3>               
-                    <h3><?php echo "Voto in decimi:"." ".$voto_originale_2."<br>"."Voto in quinti:"." ".$movie_2->getVoto();?></h3>
+                    <p><?php  echo "<strong class='text-danger'>Durata:</strong>"." ".$durata_2;?></p>               
+                    <p><?php echo "<strong class='text-danger'>Voto in decimi:</strong>"." ".$voto_originale_2."<br>"."Voto in quinti:"." ".$movie_2->getVoto();?></p>
+                    <p><?php echo "<strong class='text-danger'>Attori:</strong>"." ".implode(", ",$attori_2); ?></p>
                 </div>
                 <div class="mt-4">
                     <h2><?php  echo "Titolo:"." ".$nome_3;?></h2>
-                    <h3><?php  echo "Durata:"." ".$durata_3;?></h3>
-                    <h3><?php echo "Voto in decimi:"." ".$voto_originale_3."<br>"."Voto in quinti:"." ".$movie_3->getVoto();?></h3>
+                    <p><?php  echo "<strong class='text-danger'>Durata:</strong>"." ".$durata_3;?></p>
+                    <p><?php echo "<strong class='text-danger'>Voto in decimi:</strong>"." ".$voto_originale_3."<br>"."Voto in quinti:"." ".$movie_3->getVoto();?></p>
+                    <p><?php echo "<strong class='text-danger'>Attori:</strong>"." ".implode(", ",$attori_3); ?></p>
                 </div>
                 <div>
                     <h3 class="text-danger">
