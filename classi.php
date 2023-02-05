eef45b5e18c8d2923857ce7d354bc2b70de0a58f
LucaFaedda <lucakarl91@gmail.com>
2023-02-05 22:45:37 +0100

    test foreach

--- classi.php
+++ classi.php
@@ -37,19 +37,16 @@ Class Movies{
     $durata_1= '2h 58m';
     $voto_originale_1 = 9;
     $attori_1 = $film1;
-    //  ['Elijan Wood', 'Ian McKellen', 'Liv Tyler', 'Viggo Mortense', 'Sean Astin', 'Cate Blanchett', 'John Rhys-Davies', 'Billy Boyd', 'Dominic Monaghan', 'Orlando Bloom', 'Christoper Lee','Hugo Weaving','Sean Bean', 'Ian Holm'. 'Andy Serkis'];
     $movie_1 = new Movies($nome_1,$durata_1, $voto_originale_1, $attori_1 );
     $nome_2 = "Il signore degli Anelli: Le due Torri";
     $durata_2= '2h 59m';
     $voto_originale_2 = 9;
     $attori_2 = $film2;
-    // ['Elijan Wood', 'Ian McKellen', 'Liv Tyler', 'Viggo Mortense', 'Sean Astin', 'Cate Blanchett', 'John Rhys-Davies','Bernad Hill', 'Miranda Otto', 'Karl Urban', 'David Wenham','Brad Dourif', 'Billy Boyd', 'Dominic Monaghan', 'Orlando Bloom', 'Christoper Lee','Hugo Weaving', 'Andy Serkis'];
     $movie_2 = new Movies($nome_2, $durata_2, $voto_originale_2, $attori_2);
     $nome_3 = "Il signore degli Anelli: Il ritorno Del Re";
     $durata_3= '3h 30m';
     $voto_originale_3 = 10;
     $attori_3 = $film3;
-    // ['Elijan Wood', 'Ian McKellen', 'Liv Tyler', 'Viggo Mortense', 'Sean Astin', 'Cate Blanchett', 'John Rhys-Davies','Bernad Hill', 'Miranda Otto', 'Karl Urban', 'David Wenham','Brad Dourif', 'Billy Boyd', 'Dominic Monaghan', 'Orlando Bloom', 'Christoper Lee','Hugo Weaving', 'Andy Serkis', 'Sean Bean' , 'John Noble'];
     $movie_3 = new Movies($nome_3,$durata_3, $voto_originale_3, $attori_3);
     
     $movie_1->setVoto( $voto_originale_1); // qua posso passarli il voto originale ma solo con la freccia.
